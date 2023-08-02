3386ace820f967cce98cf9392c12bdaa2c3eac6c
HowardLee134 <m_lee134@u.pacific.edu>
2023-08-02 09:56:23 -0700

    url fix

--- src/components/work/Workitems.jsx
+++ src/components/work/Workitems.jsx
@@ -5,7 +5,7 @@ const Workitems = ({ item }) => {
     <div className='work__card' key={item.id}>
         <img src={item.image} alt='' className='work__image' />
         <h3 className='work__title'>{item.title}</h3>
-        <a href="https://github.com/" className="work__button">
+        <a href="https://github.com/HowardLee134" className="work__button">
             demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>   
     </div>
